7241db340088cdd1d628c4f2e3fd0159d147068b
Kazuhito Hokamura <k.hokamura@gmail.com>
2020-07-12 21:11:33 +0900

    update

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -17,10 +17,10 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "isucon" {
   count = 3
 
-  ami = data.aws_ami.ubuntu.id
-  # instance_type = "t3.medium"
-  instance_type = "t3.nano"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.medium"
   key_name      = "isucon"
+
 
   vpc_security_group_ids = [aws_security_group.default.id]
 
@@ -34,4 +34,43 @@ resource "aws_instance" "isucon" {
   tags = {
     Name = "isucon9-${count.index + 1}"
   }
+}
+
+resource "aws_instance" "bench" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.medium"
+  key_name      = "isucon"
+
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+  root_block_device {
+    volume_size           = 8
+    volume_type           = "gp2"
+    iops                  = 100
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "isucon9-bench"
+  }
+}
+
+resource "aws_eip" "isucon1" {
+  instance = aws_instance.isucon[0].id
+  vpc      = true
+}
+
+resource "aws_eip" "isucon2" {
+  instance = aws_instance.isucon[1].id
+  vpc      = true
+}
+
+resource "aws_eip" "isucon3" {
+  instance = aws_instance.isucon[2].id
+  vpc      = true
+}
+
+resource "aws_eip" "bench" {
+  instance = aws_instance.bench.id
+  vpc      = true
 }
