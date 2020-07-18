d81600053894fb83db31cf8cf101a9a94bbacee6
Kazuhito Hokamura <k.hokamura@gmail.com>
2020-07-18 14:27:08 +0000

    Set campaign via env

--- webapp/ruby/lib/isucari/web.rb
+++ webapp/ruby/lib/isucari/web.rb
@@ -194,7 +194,7 @@ module Isucari
 
       response = {
         # キャンペーン実施時には還元率の設定を返す。詳しくはマニュアルを参照のこと。
-        'campaign' => 2,
+        'campaign' => ENV['cp']&.to_i ,
         # 実装言語を返す
         'language' => 'ruby',
       }
